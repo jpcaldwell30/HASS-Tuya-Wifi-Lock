4d8ee226e56827d341d2881c6fedb01c7ae605b8
John Caldwell <jp.caldwell30@gmail.com>
2023-10-18 22:39:16 -0500

    new lock version test

--- custom_components/tuya/lock.py
+++ custom_components/tuya/lock.py
@@ -1,47 +1,40 @@
-"""Support for Tuya Locks"""
+"""Support for Tuya wifi lock platform."""
 from __future__ import annotations
 
-from dataclasses import dataclass
-
-import logging
 from typing import Any
 
-from homeassistant.components.lock import LockEntity, LockEntityDescription
+from tuya_iot import TuyaDevice, TuyaDeviceManager
+
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.exceptions import ServiceNotFound
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from tuya_iot import TuyaDevice, TuyaDeviceManager
 
 from . import HomeAssistantTuyaData
 from .base import TuyaEntity
-from .const import DOMAIN, TUYA_DISCOVERY_NEW, DPCode, DPType
+from .const import DOMAIN, TUYA_DISCOVERY_NEW, DPCode
 
+from homeassistant.components.lock import (
+    LockDeviceClass,
+    LockEntity,
+    LockEntityDescription,
+    LockEntityFeature
+)
 
-@dataclass
-class TuyaLockEntityDescription(LockEntityDescription):
-    open_value: str = "open"
-    closed_value: str = "closed"
-    unknown_value: str = "unknown"
-
-
-LOCKS: dict[str, TuyaLockEntityDescription] = {
+LOCKS: dict[str, tuple[LockEntityDescription, ...]] = {
     "jtmsbh": (
         LockEntityDescription(
             key="lock_motor_state",
-            icon="mdi:lock"
+            icon="mdi:lock",
         )
-    )
+    ),
 }
 
-_LOGGER = logging.getLogger(__name__)
-
-
 async def async_setup_entry(
-        hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """Set up tuya lock dynamically through tuya discovery."""
+    """Set up tuya sensors dynamically through tuya discovery."""
     hass_data: HomeAssistantTuyaData = hass.data[DOMAIN][entry.entry_id]
 
     @callback
@@ -50,8 +43,14 @@ async def async_setup_entry(
         entities: list[TuyaLockEntity] = []
         for device_id in device_ids:
             device = hass_data.device_manager.device_map[device_id]
-            if description := LOCKS.get(device.category):
-                entities.append(TuyaLockEntity(device, hass_data.device_manager, description))
+            if descriptions := LOCKS.get(device.category):
+                for description in descriptions:
+                    if description.key in device.status:
+                        entities.append(
+                            TuyaLockEntity(
+                                device, hass_data.device_manager, description
+                            )
+                        )
 
         async_add_entities(entities)
 
@@ -63,35 +62,28 @@ async def async_setup_entry(
 
 
 class TuyaLockEntity(TuyaEntity, LockEntity):
-    _closed_opened_dpcode: DPCode | None = None
-    entity_description: TuyaLockEntityDescription | None = None
+    """Tuya Lock Device."""
 
     def __init__(
-            self,
-            device: TuyaDevice,
-            device_manager: TuyaDeviceManager,
-            description: TuyaLockEntityDescription
+        self,
+        device: TuyaDevice,
+        device_manager: TuyaDeviceManager,
+        description: LockEntityDescription,
     ) -> None:
+        """Init TuyaHaLock."""
         super().__init__(device, device_manager)
-
         self.entity_description = description
-
-        if enum_type := self.find_dpcode(DPCode.CLOSED_OPENED, dptype=DPType.ENUM):
-            self._closed_opened_dpcode = enum_type.dpcode
+        self._attr_unique_id = f"{super().unique_id}{description.key}"
 
     @property
-    def is_locked(self) -> bool | None:
-        """Return true if the lock is locked."""
-        status = self.device.status.get(self._closed_opened_dpcode)
-        if status == self.entity_description.unknown_value:
-            return None
+    def is_locked(self) -> bool:
+        """Return true if lock is on."""
+        return self.device.status.get(self.entity_description.key, False)
 
-        return status == self.entity_description.closed_value
+    def lock(self, **kwargs):
+        """Lock the lock."""
+        self._send_command([{"code": self.entity_description.key, "value": True}]) #tuya  method
 
-    def lock(self, **kwargs: Any) -> None:
-        _LOGGER.warning("Trying to lock. This operation is not supported.")
-        raise ServiceNotFound(self.device.category, "lock")
-
-    def unlock(self, **kwargs: Any) -> None:
-        _LOGGER.warning("Trying to unlock. This operation is not supported")
-        raise ServiceNotFound(self.device.category, "unlock")
+    def unlock(self, **kwargs):
+        """Unlock the lock."""
+        self._send_command([{"code": self.entity_description.key, "value": False}]) #tuya  method
